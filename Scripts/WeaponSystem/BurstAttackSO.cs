19e09f2fb8cd7752d2cc0a2b5e347097b4077172
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2022-02-21 13:02:04 +0100

    Section 3 Vid 6 Burst Attack Pattern

---
+++ Scripts/WeaponSystem/BurstAttackSO.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SVS.WeaponSystem
+{
+    [CreateAssetMenu(menuName = "Attacks/BurstAttack")]
+    public class BurstAttackSO : AttackPatternSO
+    {
+        [SerializeField]
+        private GameObject projectile;
+
+        [SerializeField]
+        private float offset = 0.1f;
+        public override void Perform(Transform shootingStartPoint)
+        {
+            Vector3 offsetDirection = shootingStartPoint.rotation * new Vector3(0, offset, 0);
+            Instantiate(projectile, shootingStartPoint.position, shootingStartPoint.rotation);
+            Instantiate(projectile, shootingStartPoint.position + offsetDirection, shootingStartPoint.rotation);
+            Instantiate(projectile, shootingStartPoint.position - offsetDirection, shootingStartPoint.rotation);
+        }
+    }
+}
