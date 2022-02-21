a29ea05214e4d8c10db28ae2baaa2acd60f9e3a9
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2022-02-21 12:54:54 +0100

    Section 3 Vid 5 Double bullet and Spread bullet patterns

---
+++ Scripts/WeaponSystem/DoubleBarrelAttackSO.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SVS.WeaponSystem
+{
+    [CreateAssetMenu(menuName = "Attacks/DoubleBarrelAttack")]
+    public class DoubleBarrelAttackSO : AttackPatternSO
+    {
+        [SerializeField]
+        private GameObject projectile;
+        [SerializeField]
+        private float offsetFromShootingPoint = 0.3f;
+
+        public override void Perform(Transform shootingStartPoint)
+        {
+            //rotate offset vector to fit the players ship rotation
+            Vector3 offsetVector = shootingStartPoint.rotation * 
+                new Vector3(offsetFromShootingPoint, 0, 0);
+
+            //calculate new barrel positions
+            Vector3 point1 = shootingStartPoint.position + offsetVector;
+            Vector3 point2 = shootingStartPoint.position - offsetVector;
+
+            Instantiate(projectile, point1, shootingStartPoint.rotation);
+            Instantiate(projectile, point2, shootingStartPoint.rotation);
+        }
+    }
+}
---
+++ Scripts/WeaponSystem/SpreadAttackSO.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SVS.WeaponSystem
+{
+    [CreateAssetMenu(menuName = "Attacks/SpreadAttack")]
+    public class SpreadAttackSO : AttackPatternSO
+    {
+        [SerializeField]
+        private GameObject projectile;
+
+        [SerializeField]
+        private float angleDegrees = 5;
+        public override void Perform(Transform shootingStartPoint)
+        {
+            Instantiate(projectile, shootingStartPoint.position, shootingStartPoint.rotation);
+            Instantiate(projectile, shootingStartPoint.position, shootingStartPoint.rotation 
+                * Quaternion.Euler(Vector3.forward * angleDegrees));
+            Instantiate(projectile, shootingStartPoint.position, shootingStartPoint.rotation 
+                * Quaternion.Euler(Vector3.forward * -angleDegrees));
+        }
+    }
+}
